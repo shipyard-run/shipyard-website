5e6f706cb88fd25530b3517c2c0b924f68d2ad95
Nicholas Jackson <jackson.nic@gmail.com>
2021-01-13 10:47:24 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,14 +54,6 @@ function Home() {
             </div>
           </div>
         </div>
-        <iframe 
-          src="https://discordapp.com/widget?id=771742803196772402&theme=dark" 
-          width="350" 
-          height="500"  
-          allowtransparency="true" 
-          frameborder="0" 
-          sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts">
-        </iframe>
       </main>
     </Layout>
   );
